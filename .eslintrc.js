a3e0f4ad8c86bf9dbdb1e2a0efb25e7ff2488ea5
Andreas Nel <Andreas.Nel@standardbank.co.za>
2023-08-11 10:59:42 +0200

    Add linting, formatting and missing libs

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,25 +2,29 @@ module.exports = {
   env: {
     browser: true,
     commonjs: true,
-    es2021: true
+    es2021: true,
   },
-  extends: '@eslint/recommended',
+  extends: ["eslint:recommended", "prettier"],
+  plugins: ["jest"],
   overrides: [
     {
       env: {
-        node: true
+        node: true,
       },
-      files: [
-        '.eslintrc.{js,cjs}'
-      ],
+      files: [".eslintrc.{js,cjs}"],
       parserOptions: {
-        sourceType: 'script'
-      }
-    }
+        sourceType: "script",
+      },
+    },
+    {
+      env: { "jest/globals": true },
+      files: ["test.js"],
+    },
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
+    ecmaVersion: "latest",
   },
   rules: {
-  }
-}
+    quotes: "error",
+  },
+};
